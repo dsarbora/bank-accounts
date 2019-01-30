bc6f84e2dd4bcacb0390d8c896342fbe1bb583b2
Dave Sarbora and Jasmine Lee <dave@>
2019-01-29 16:42:18 -0800

    create additional JS logic

--- js/scripts.js
+++ js/scripts.js
@@ -14,26 +14,72 @@ AccountLog.prototype.assignID = function(){
   return this.currentID;
 };
 
+AccountLog.prototype.findAccount = function(accountID){
+  for(var i = 0; i < this.accounts.length; i++){
+    if(this.accounts[i]){
+      if(this.accounts[i].id==accountID){
+        return this.accounts[i];
+      }
+    }
+  };
+  return false;
+};
+
+AccountLog.prototype.deleteAccount = function(accountID){
+  for(var i = 0; i < this.accounts.length; i++){
+    if(this.accounts[i]){
+      if(this.accounts[i].id==accountID){
+        delete this.accounts[i];
+        return true;
+      }
+    }
+  };
+  return false;
+};
+
+
 //logic for BankAccount
 function BankAccount(name, amount){
   this.name = name,
   this.amount = amount
 };
 
-BankAccount.prototype.withdrawal = function(number){
-  this.amount -= number;
+BankAccount.prototype.withdrawal = function(withdrawal){
+  this.amount -= parseInt(withdrawal);
+  return this.amount;
 };
 
-BankAccount.prototype.deposit = function(number){
-  this.amount += number;
+BankAccount.prototype.deposit = function(deposit){
+  this.amount += parseInt(deposit);
+  return this.amount;
 };
 
 //front end logic
 var accountLog = new AccountLog();
 
-function showAccount(){
+function showAccountNames(accountLogToBeDisplayed){
+
 
 }
+
+function showAccount(){
+  var accountsList = $("ul#show-account");
+  var htmlForAccountsList = "";
+  accountLog.accounts.forEach(function(account){
+    htmlForAccountsList += "<li id =" + account.id + ">"+ account.amount + "</li>"
+  });
+  accountsList.html(htmlForAccountsList);
+};
+
+function showBalance(accountID){
+  $("#balance").text(accountLog.findAccount(accountID).amount);
+}
+
+function attachAccountListenters(){
+  $("ul#show-account").on("click", "li", function(){
+    showBalance(this.id);
+  });
+};
 
 
 $(function(){
@@ -41,11 +87,21 @@ $(function(){
   $("#new-account").submit(function(event) {
     event.preventDefault();
     var name = $("#name").val();
-    var amount = $("#initial-deposit").val();
+    var amount = parseInt($("#initial-deposit").val());
     var account = new BankAccount(name, amount);
     accountLog.addAccount(account)
-    showAccount(account);
+    showAccount(accountLog);
+    attachAccountListenters();
+  });
 
+  $("#acctChange").submit(function(){
+    event.preventDefault();
+    debugger;
+    var withdrawal = $("#withdrawal").val();
+    //var deposit = $("#deposit").val();
+    accountLog.accounts[0].withdrawal(withdrawal);
+    //accountLog.accounts[0].deposit(deposit);
+    showBalance(1);
   });
 
 
